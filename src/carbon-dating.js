ee8e05e662871c6a544de4bb22db8438f442e655
Bella Makhova <rgvder@gmail.com>
2022-04-29 01:30:07 +0300

    feat: solved carbon dating, hanoi tower

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,13 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  if (!sampleActivity || typeof sampleActivity !== 'string'|| !(/^\d+\.*\d*$/.test(sampleActivity)) || +sampleActivity >= 15 || +sampleActivity <= 0) {
+    return false;
+  } else {
+    return Math.ceil(Math.log(MODERN_ACTIVITY / +sampleActivity) / k);
+  }
 }
 
 module.exports = {
--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,9 +14,15 @@ const { NotImplementedError } = require('../extensions/index.js');
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function calculateHanoi(disks, turnsSpeed) {
+  let obj = {
+    turns: null,
+    seconds: null
+  }
+
+  obj.turns = 2 ** disks - 1;
+  obj.seconds = Math.floor((obj.turns / turnsSpeed) * 3600);
+  return obj;
 }
 
 module.exports = {
